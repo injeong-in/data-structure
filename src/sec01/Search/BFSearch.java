8aef1aff6a5733b78c2e1d563468f3979ecfc2b9
injeong-in <goalddaerinae@gmail.com>
2020-07-16 16:10:32 +0900

    update

---
+++ src/sec01/Search/BFSearch.java
@@ -0,0 +1,94 @@
+//package sec01.Search;
+//
+//import java.util.LinkedList;
+//import java.util.Queue;
+//import java.util.Stack;
+//
+//public class BFSearch {
+//
+//
+//
+//
+//}
+//
+//class Graph {
+//    class Node {
+//        int data;
+//        boolean marked;
+//        LinkedList<Node> adjacent;
+//
+//        Node(int data) {
+//            this.data = data;
+//            this.marked = false;
+//            adjacent = new LinkedList<>();
+//        }
+//    }
+//
+//    //그래프 만들기
+//    Node[] nodes;
+//    public Graph(int size) {
+//        nodes = new Node[size];
+//        for (int i = 0; i < size; i++) {
+//            nodes[i] = new Node(i);
+//        }
+//}
+//    //두 노드의 관계를 지어주는 함수
+//    void addEdge(int i1, int i2) {
+//        Node n1 = nodes[i1];
+//        Node n2 = nodes[i2];
+//        if(!n1.adjacent.contains(n2)) {
+//            n1.adjacent.add(n2);
+//        }
+//        if(!n2.adjacent.contains(n1)) {
+//            n2.adjacent.add(n1);
+//        }
+//    }
+//    void dfs() {
+//        dfs(0);
+//    }
+//
+//    void dfs(int index) {
+//        Node root = nodes[index];
+//        Stack<Node> stack = new Stack<>();
+//        stack.push(root);
+//        root.marked = true;
+//        while(!stack.isEmpty()) {
+//            Node r = stack.pop();
+//            for(Node n : r.adjacent) {
+//                if(n.marked == false) {
+//                    n.marked = true;
+//                    stack.push(n);
+//                }
+//            }
+//            visit(r);
+//        }
+//    }
+//
+//    void bfs() {
+//        bfs(0);
+//    }
+//
+//    void bfs(int index) {
+//        Node root = nodes[index];
+//        Queue<Node> queue = new Queue<Node>();
+//        queue.enqueue(root);
+//        root.marked = true;
+//        while(!queue.isEmpty()) {
+//            Node r = queue.dequeue();
+//            for(Node n : r.adjacent) {
+//                if(n.marked == false) {
+//                    n.marked = true;
+//                    queue.enqueue(n);
+//                }
+//            }
+//            visit(r);
+//        }
+//    }
+//
+//
+//
+//    void visit(Node n) {
+//        System.out.println(n.data + " ");
+//        }
+//    }
+//
---
+++ src/sec01/Search/Test.java
@@ -0,0 +1,21 @@
+package sec01.Search;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Queue<Integer> q = new Queue<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.peek());
+        System.out.println(q.remove());
+        System.out.println(q.isEmpty());
+
+    }
+
+}
+
+
