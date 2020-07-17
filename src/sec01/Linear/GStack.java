3220116da43552b76ec88dbca96e02b0d386b7fd
injeong-in <goalddaerinae@gmail.com>
2020-07-17 16:31:49 +0900

    update comment to GStack

--- src/sec01/Linear/GStack.java
+++ src/sec01/Linear/GStack.java
@@ -1,8 +1,9 @@
 package sec01.Linear;
 
+//제네릭을 활용한 스택
 public class GStack<T> {
 
-    int tos;
+    int tos; //1번부터 시작하는 정수값(인덱스 넘버와 별개!)
     Object[] stck; //스택에 요소를 저장할 공간 배열
 
     public GStack() {
@@ -10,14 +11,16 @@ public class GStack<T> {
         stck = new Object [10];
     }
 
+    //삽입용 메서드
     public void push(T item) {
         if(tos == 10)
             return;
 //        tos++;
-        stck[tos] = item;
-        tos++;
+        stck[tos] = item; //0번 인덱스
+        tos++; //반복문에서 사용할 순서 정수값(1부터 시작)
     }
 
+    //출력용 메서드
     public T pop() {
         if(tos==0)
             return null;
@@ -33,7 +36,6 @@ public class GStack<T> {
 
 
         for(int i=0; i<3; i++) {
-//            if(stringGStack.pop() == null) return;
             System.out.println(stringGStack.tos);
             System.out.println(stringGStack.pop());
         }
